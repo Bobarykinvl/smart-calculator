e0b28760cbbd22aa048278f981e23f281d0eb6d0
Bobarykinvl <bobarykinvl@gmail.com>
2018-03-13 13:25:57 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,27 +1,121 @@
 class SmartCalculator {
   constructor(initialValue) {
-    // your implementation
+	this.exampleZn = ['**','/','*','-','+'];
+	this.initialValue =  initialValue;
+	var answer = 0;
+    this.rezArr = [];
+	this.rezArr[0] = initialValue;
   }
 
   add(number) {
-    // your implementation
+    this.rezArr.push('+');
+    this.rezArr.push(number);
+    return this;
   }
   
   subtract(number) {
-    // your implementation
+    this.rezArr.push('-');
+    this.rezArr.push(number);
+    return this;
   }
 
   multiply(number) {
-    // your implementation
+    this.rezArr.push('*');
+    this.rezArr.push(number);
+    return this;
   }
 
   devide(number) {
-    // your implementation
+    this.rezArr.push('/');
+    this.rezArr.push(number);
+    return this;
   }
 
   pow(number) {
-    // your implementation
+    this.rezArr.push('**');
+    this.rezArr.push(number);
+    return this;
   }
+	
+	
+  toString(){
+	  
+	  
+	var examLength = this.exampleZn.length;
+    for(var i=0; i < examLength; i++){
+      	for(var k=0; k < this.rezArr.length; k++){
+//			console.log(this.exampleZn[i]);
+			if(this.rezArr[k] == this.exampleZn[i]){
+				if(this.rezArr[k] == '**') {
+//				  console.log('пров');
+				  this.rezArr[k] = Math.pow(this.rezArr[k-1],this.rezArr[k+1]);
+				  this.rezArr.splice(k + 1, 1);
+				  this.rezArr.splice(k - 1, 1);
+				  k = 0;
+				}
+			}
+		}
+	}
+	  
+	  
+	  for(var i=0; i < examLength; i++){
+      	for(var k=0; k < this.rezArr.length; k++){
+			if(this.rezArr[k]== this.exampleZn[i]){
+				if(this.rezArr[k] == '*'){
+				  this.rezArr[k] = this.rezArr[k+1]*this.rezArr[k-1];
+				  this.rezArr.splice(k + 1, 1);
+				  this.rezArr.splice(k - 1, 1);
+				  k = 0;
+				}
+			}
+		}
+	  }
+	  
+	  
+	  for(var i=0; i < examLength; i++){
+      	for(var k=0; k < this.rezArr.length; k++){
+			if(this.rezArr[k]== this.exampleZn[i]){
+				if(this.rezArr[k] == '/'){
+				  this.rezArr[k] = this.rezArr[k-1]/this.rezArr[k+1];
+				  this.rezArr.splice(k + 1, 1);
+				  this.rezArr.splice(k - 1, 1);
+				  k = 0;
+				}
+			}
+		}
+	  }
+	  
+	  
+	  for(var i=0; i < examLength; i++){
+      	for(var k = 0; k<this.rezArr.length; k++){
+			if(this.rezArr[k]== this.exampleZn[i]){
+				if(this.rezArr[k] == '-'){
+				  this.rezArr[k] = this.rezArr[k-1]-this.rezArr[k+1];
+				  this.rezArr.splice(k + 1, 1);
+				  this.rezArr.splice(k - 1, 1);
+				  k = 0;
+				}
+			}
+		}
+	  }
+	  
+	  
+	   for(var i=0; i < examLength; i++){
+      	for(var k = 0; k < this.rezArr.length; k++){
+			if(this.rezArr[k]== this.exampleZn[i]){
+				if(this.rezArr[k] == '+'){
+				  this.rezArr[k] = this.rezArr[k+1]+this.rezArr[k-1];
+				  this.rezArr.splice(k + 1, 1);
+				  this.rezArr.splice(k - 1, 1);
+				  k = 0;
+				}
+		}
+      }
+    }
+	
+    return this.rezArr[0];
+  }
+
 }
 
 module.exports = SmartCalculator;
